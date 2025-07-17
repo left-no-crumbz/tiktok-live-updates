33221fd425cdc819b98bc535c4307afc21013f76
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2025-07-17 12:56:06 +0800

    feat(backend): Add express, cheerio, and axios as dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ function App() {
       <div className='flex flex-col items-center justify-center w-screen h-screen'>
         <h1 className='text-center text-4xl md:text-6xl'>Tiktok Live Updates</h1>
         <br></br>
-        <input type="text" placeholder="Enter TikTok live link here" className="input"/>
+        <input type="text" placeholder="Enter TikTok live link here" className="input" onChange={(e) => setLiveLink(e.target.value)}/>
+        <br></br>
         <p>{liveLink}</p>
       </div>
     </>
